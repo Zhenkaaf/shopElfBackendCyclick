b2f83f7997d65e4be997dfd4ce4ed27335102f07
Zhenkaaf <zhenkaaf@gmail.com>
2023-05-31 15:09:50 +0300

    timestamps

--- models/orderSchema.js
+++ models/orderSchema.js
@@ -1,33 +1,36 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const orderSchema = new Schema({
-  name: {
-    type: String,
-    required: false,
-  },
-  email: {
-    type: String,
-    required: false,
-  },
-  phone: {
-    type: String,
-    required: false,
-  },
-  address: {
-    type: String,
-    required: false,
-  },
-  products: [
-    {
-      name: String,
-      quantity: Number,
+const orderSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: false,
     },
-  ],
-  totalPrice: {
-    type: Number,
-    required: true,
+    email: {
+      type: String,
+      required: false,
+    },
+    phone: {
+      type: String,
+      required: false,
+    },
+    address: {
+      type: String,
+      required: false,
+    },
+    products: [
+      {
+        name: String,
+        quantity: Number,
+      },
+    ],
+    totalPrice: {
+      type: Number,
+      required: true,
+    },
   },
-});
+  { timestamps: true }
+);
 
 const Order = mongoose.model("Order", orderSchema);
 
